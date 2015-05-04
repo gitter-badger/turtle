269c806aabf7c358da8d34d18ee5cd1eb9c57bd9
Roland Singer <roland.singer@desertbit.com>
2015-05-04 03:40:31 +0200

    further progress on installation scripts

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ docker pull desertbit/turtle
 
 # Install the files.
 cp -f ./scripts/turtle.service /etc/systemd/system/turtle.service
-cp -f ./scripts/turtle /bin/turtle
-chmod +x /bin/turtle
+cp -f ./scripts/host/turtle /bin/turtle
+cp -f ./scripts/host/turtle-docker /bin/turtle-docker
+chmod +x /bin/turtle /bin/turtle-docker
 
 # Add the turtle user.
 useradd --no-create-home --no-user-group --shell /bin/turtle turtle
 
-# Enable and start the turtle service.
-systemctl enable turtle
+# Allow to run the turtle-docker command with sudo for the turtle user.
+echo "turtle ALL = (root) NOPASSWD: /bin/turtle-docker" > /etc/sudoers.d/turtle
+
+# Start the turtle service.
 systemctl start turtle
