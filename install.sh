bf2e7b0ce7c39036b940e560225181c62f5bd379
Roland Singer <roland.singer@desertbit.com>
2015-05-04 03:25:03 +0200

    fixed bug in systemd service file

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ docker pull desertbit/turtle
 # Install the files.
 cp -f ./scripts/turtle.service /etc/systemd/system/turtle.service
 cp -f ./scripts/turtle /bin/turtle
+chmod +x /bin/turtle
 
 # Add the turtle user.
 useradd --no-create-home --no-user-group --shell /bin/turtle turtle
